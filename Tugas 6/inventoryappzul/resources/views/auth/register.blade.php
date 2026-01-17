7865db1ac5fd3dabd051fecbfa0f35147ed9cd35
Zulfikar <zaxesq12@gmail.com>
2026-01-17 17:31:25 +0700

    Selesai

--- Tugas 6/inventoryappzul/resources/views/auth/register.blade.php
+++ Tugas 6/inventoryappzul/resources/views/auth/register.blade.php
@@ -20,9 +20,9 @@
           <div class="col-md-8 col-lg-6 col-xxl-3">
             <div class="card mb-0">
               <div class="card-body">
-                <a href="#" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                  <img src="{{ asset('template/src/assets/images/logos/logo-light.svg') }}" alt="">
-                </a>
+                <div class="text-nowrap logo-img text-center d-block py-3 w-100">
+                <img src="{{ asset('template/src/assets/images/logos/logologin.jpg') }}" alt="">
+                </div>
                 <p class="text-center">Make Your Account</p>
                 <form action="/register" method="POST">
                     @csrf
